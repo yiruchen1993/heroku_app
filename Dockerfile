25a196fe22d420483adb9a9b3025d0bcaf8482ad
yiruchen1993 <yiruchen1993@gmail.com>
2021-01-11 20:25:51 +0800

    fix: add .env to dockerfile, add port to py code

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,6 @@ ENV TZ=Asia/Taipei
 # both files are explicitly required!
 ADD linebot_backend.py ./
 ADD requirements.txt ./
+ADD .env ./
 RUN pip install -r ./requirements.txt
 CMD ["python", "linebot_backend.py"]
--- linebot_backend.py
+++ linebot_backend.py
@@ -12,8 +12,8 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-channel_token = os.getenv('iwxjpGo33oFAcPliGCZhvKFag7Vd+yEYi/ekML5anCUh1yxs2hSJoKCo6j6M0rfi/xWFtD2NWbCtnEeMO2g/lckM83sMBDvlWslVamH5uZgNKEIlvkyrLgK416LXYEHHonMfl8kRFhHsFhR6UouM0gdB04t89/1O/w1cDnyilFU=')
-secret = os.getenv('d913ea5f20d6456d0e78cdb9bdb8859f')
+channel_token = os.getenv('YOUR_CHANNEL_ACCESS_TOKEN')
+secret = os.getenv('YOUR_CHANNEL_SECRET')
 
 app = Flask(__name__)
 line_bot_api = LineBotApi(channel_token)
@@ -42,10 +42,11 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     # message = TextSendMessage(text=event.message.text)
-    message = TextSendMessage(text='Hello World From Container!')
+    message = TextSendMessage(text='Hello, world!')
     line_bot_api.reply_message(event.reply_token, message)
 
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+    # app.run()
