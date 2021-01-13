61852207bc88f2ed95c74da54101f44c1010b915
yiruchen1993 <yiruchen1993@gmail.com>
2021-01-13 21:00:42 +0800

    feat: save input message to database

--- linebot_backend.py
+++ linebot_backend.py
@@ -10,14 +10,29 @@ from linebot.models import (
 )
 from dotenv import load_dotenv
 import os
+import psycopg2
+from sqlalchemy import create_engine
+
 
 load_dotenv()
 channel_token = os.getenv('YOUR_CHANNEL_ACCESS_TOKEN')
 secret = os.getenv('YOUR_CHANNEL_SECRET')
+host = os.getenv('dbhost')
+dbname = os.getenv('dbname')
+user = os.getenv('dbuser')
+port = os.getenv('dbport')
+password = os.getenv('dbpassword')
+db_info = {'user': user, 'password': password, 'host': host, 'port': port, 'dbname': dbname}
+# print(db_info)
 
 app = Flask(__name__)
 line_bot_api = LineBotApi(channel_token)
 handler = WebhookHandler(secret)
+
+
+@app.route('/')
+def index():
+    return "hello this is linebotjobfinder"
 
 
 @app.route("/callback", methods=['POST'])
@@ -41,9 +56,43 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    # message = TextSendMessage(text=event.message.text)
-    message = TextSendMessage(text='Hello, world!')
-    line_bot_api.reply_message(event.reply_token, message)
+    userid = event.source.user_id
+    input_message = event.message.text
+    app.logger.info("user_id: ", userid)
+    app.logger.info("Handle: reply_token: " + event.reply_token + ", message: " + input_message)
+    content = "{}: {}".format(event.source.user_id, event.message.text)
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=content))
+    save_to_db(userid, input_message)
+
+
+def save_to_db(userid, input_message):
+    pg_conn = connect_pg_db(db_info)
+    app.logger.info(pg_conn)
+    msg_list = input_message.split()
+    location = msg_list[0]
+    position = msg_list[1]
+    sql_ = "select * from insert_registration('{}', '{}', '{}')".format(userid, location, position)
+    app.logger.info(sql_)
+    with pg_conn.begin() as a:
+        pg_conn.execute(sql_)
+    pg_conn.close()
+
+
+def connect_pg_db(db_info):
+    conn_str = "{}://{}:{}@{}:{}/{}".format(
+        "postgresql+psycopg2",
+        db_info["user"],
+        db_info["password"],
+        db_info["host"],
+        db_info["port"],
+        db_info["dbname"],
+    )
+    alchemyEngine = create_engine(conn_str, pool_recycle=3600)
+    pg_connection = alchemyEngine.connect()
+    return pg_connection
+
 
 
 if __name__ == "__main__":
