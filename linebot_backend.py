be6b25b7560714429c6214d88bb2382e2314adf3
yiruchen1993 <yiruchen1993@gmail.com>
2021-01-30 12:01:30 +0800

    feat: add cancel function and error handling of input message

--- linebot_backend.py
+++ linebot_backend.py
@@ -11,6 +11,7 @@ from linebot.models import (
 from dotenv import load_dotenv
 import os
 import psycopg2
+import pandas as pd
 from sqlalchemy import create_engine
 
 
@@ -58,13 +59,34 @@ def callback():
 def handle_message(event):
     userid = event.source.user_id
     input_message = event.message.text
+    msg_list = input_message.split()
     app.logger.info("user_id: ", userid)
     app.logger.info("Handle: reply_token: " + event.reply_token + ", message: " + input_message)
-    content = "{}: {}".format(event.source.user_id, event.message.text)
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=content))
-    save_to_db(userid, input_message)
+    # content = "{}: {}".format(event.source.user_id, event.message.text)
+    # line_bot_api.reply_message(event.reply_token, TextSendMessage(text=content))
+    area_list = get_area_list()
+    try:
+        if input_message.lower() == "cancel":
+            cancel_register(userid)
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text=input_message))
+        elif (len(msg_list) >= 2) & (msg_list[0] in area_list):
+            save_to_db(userid, input_message)
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text=input_message))
+        else:
+            msg = "please follow the registration rule"
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text=msg))
+    except Exception as e:
+        print("error:", e)
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text="Error"))
+
+
+def get_area_list():
+    pg_conn = connect_pg_db(db_info)
+    sql_ = "select target_location from location_code_ct"
+    row = pg_conn.execute(sql_)
+    area_list = pd.DataFrame(row.fetchall(), columns=row.keys())
+    pg_conn.close()
+    return area_list.target_location.values
 
 
 def save_to_db(userid, input_message):
@@ -72,8 +94,18 @@ def save_to_db(userid, input_message):
     app.logger.info(pg_conn)
     msg_list = input_message.split()
     location = msg_list[0]
-    position = msg_list[1]
+    position = " ".join(msg_list[1:])
     sql_ = "select * from insert_registration('{}', '{}', '{}')".format(userid, location, position)
+    app.logger.info(sql_)
+    with pg_conn.begin() as a:
+        pg_conn.execute(sql_)
+    pg_conn.close()
+
+
+def cancel_register(userid):
+    pg_conn = connect_pg_db(db_info)
+    app.logger.info(pg_conn)
+    sql_ = "delete from registration_ct where user_id = '{}'".format(userid)
     app.logger.info(sql_)
     with pg_conn.begin() as a:
         pg_conn.execute(sql_)
